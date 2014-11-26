0c6e79d2d3317ff850ee0ec6594dcc93de465bba
siddharth singh <01siddharthsingh@gmail.com>
2014-11-26 21:08:29 +0530

    First Commit

---
+++ ThinkUino/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ThinkUino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnHeadsetConnect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblMeditation = new System.Windows.Forms.Label();
+            this.lblAttention = new System.Windows.Forms.Label();
+            this.txtHeadsetDebug = new System.Windows.Forms.TextBox();
+            this.btnHeadsetDisconnect = new System.Windows.Forms.Button();
+            this.serialArduino = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.blink = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnHeadsetConnect
+            // 
+            this.btnHeadsetConnect.Location = new System.Drawing.Point(63, 33);
+            this.btnHeadsetConnect.Name = "btnHeadsetConnect";
+            this.btnHeadsetConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnHeadsetConnect.TabIndex = 0;
+            this.btnHeadsetConnect.Text = "Connect";
+            this.btnHeadsetConnect.UseVisualStyleBackColor = true;
+            this.btnHeadsetConnect.Click += new System.EventHandler(this.btnHeadsetConnect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMeditation);
+            this.groupBox1.Controls.Add(this.lblAttention);
+            this.groupBox1.Controls.Add(this.txtHeadsetDebug);
+            this.groupBox1.Controls.Add(this.btnHeadsetDisconnect);
+            this.groupBox1.Controls.Add(this.btnHeadsetConnect);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 190);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Headset";
+            // 
+            // lblMeditation
+            // 
+            this.lblMeditation.AutoSize = true;
+            this.lblMeditation.Location = new System.Drawing.Point(89, 171);
+            this.lblMeditation.Name = "lblMeditation";
+            this.lblMeditation.Size = new System.Drawing.Size(59, 13);
+            this.lblMeditation.TabIndex = 4;
+            this.lblMeditation.Text = "Meditation:";
+            // 
+            // lblAttention
+            // 
+            this.lblAttention.AutoSize = true;
+            this.lblAttention.Location = new System.Drawing.Point(6, 171);
+            this.lblAttention.Name = "lblAttention";
+            this.lblAttention.Size = new System.Drawing.Size(52, 13);
+            this.lblAttention.TabIndex = 3;
+            this.lblAttention.Text = "Attention:";
+            // 
+            // txtHeadsetDebug
+            // 
+            this.txtHeadsetDebug.Location = new System.Drawing.Point(6, 101);
+            this.txtHeadsetDebug.Multiline = true;
+            this.txtHeadsetDebug.Name = "txtHeadsetDebug";
+            this.txtHeadsetDebug.Size = new System.Drawing.Size(188, 70);
+            this.txtHeadsetDebug.TabIndex = 2;
+            // 
+            // btnHeadsetDisconnect
+            // 
+            this.btnHeadsetDisconnect.Enabled = false;
+            this.btnHeadsetDisconnect.Location = new System.Drawing.Point(63, 62);
+            this.btnHeadsetDisconnect.Name = "btnHeadsetDisconnect";
+            this.btnHeadsetDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnHeadsetDisconnect.TabIndex = 1;
+            this.btnHeadsetDisconnect.Text = "Disconnect";
+            this.btnHeadsetDisconnect.UseVisualStyleBackColor = true;
+            this.btnHeadsetDisconnect.Click += new System.EventHandler(this.btnHeadsetDisconnect_Click);
+            // 
+            // serialArduino
+            // 
+            this.serialArduino.PortName = "null";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Antonio Blescia - www.blesciasw.it";
+            // 
+            // blink
+            // 
+            this.blink.Location = new System.Drawing.Point(12, 209);
+            this.blink.Name = "blink";
+            this.blink.Size = new System.Drawing.Size(201, 20);
+            this.blink.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 316);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.blink);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ThinkUino";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHeadsetConnect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtHeadsetDebug;
+        private System.Windows.Forms.Button btnHeadsetDisconnect;
+        private System.Windows.Forms.Label lblAttention;
+        private System.Windows.Forms.Label lblMeditation;
+        private System.IO.Ports.SerialPort serialArduino;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox blink;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ ThinkUino/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ThinkGearNET;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ThinkUino
+{
+    public partial class Form1 : Form
+    {
+
+        string ip = "192.168.43.63";
+        bool check = false;
+
+        private ThinkGearWrapper thinkGearWrapper = new ThinkGearWrapper(); //declare the thinkgear object
+       
+
+
+        private const string MINDPORT = "COM6"; //declare the Mindwave serial port
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHeadsetConnect_Click(object sender, EventArgs e)
+        {
+            if (thinkGearWrapper.Connect(MINDPORT, 57600, true)) //check if the Mindwave is connected
+            {
+                btnHeadsetConnect.Enabled = false; //disable connect button
+                btnHeadsetDisconnect.Enabled = true; //enable disconnect button
+                txtHeadsetDebug.AppendText("Headset connected." + Environment.NewLine);
+                thinkGearWrapper.EnableBlinkDetection(true); //enable the eye blink on the eSense protocol
+                thinkGearWrapper.ThinkGearChanged += new EventHandler<ThinkGearChangedEventArgs>(thinkGearWrapper_ThinkGearChanged); //capture the event when a new data is incoming
+            }
+            else
+                txtHeadsetDebug.AppendText("Could not connect to headset" + Environment.NewLine);
+        }
+
+        void thinkGearWrapper_ThinkGearChanged(object sender, ThinkGearChangedEventArgs e)
+        {
+
+
+            BeginInvoke(new MethodInvoker(delegate //use the AsyncTask to update the UI
+                {
+
+
+                    if ((thinkGearWrapper.ThinkGearState.BlinkStrength > 30)) // check if the eye blink strength is above 30
+                    {
+
+                        txtHeadsetDebug.AppendText(System.Environment.NewLine + thinkGearWrapper.ThinkGearState.BlinkStrength.ToString());
+
+
+                        if ((thinkGearWrapper.ThinkGearState.BlinkStrength > 60)) // check if the eye blink strength is above 60
+                        {
+
+
+                            sendBlink(thinkGearWrapper.ThinkGearState.BlinkStrength.ToString());
+
+
+                            thinkGearWrapper.ThinkGearState.BlinkStrength = 0;//set the eye blink strength to zero
+                        }
+                    }
+
+                    if (thinkGearWrapper.ThinkGearState.Attention > 30)//check if the Attention state is above 60
+                    {
+                        txtHeadsetDebug.AppendText(System.Environment.NewLine + thinkGearWrapper.ThinkGearState.Attention.ToString());
+
+                        if (thinkGearWrapper.ThinkGearState.Attention > 60)//check if the Attention state is above 60
+                        {
+
+                            thinkGearWrapper.ThinkGearState.Attention = 0; //set the attention state to zero
+                        }
+                    }
+                    lblAttention.Text = "Attention: " + thinkGearWrapper.ThinkGearState.Attention;
+                    lblMeditation.Text = "Meditation: " + thinkGearWrapper.ThinkGearState.Meditation;
+                }));
+            Thread.Sleep(10); //wait ten milliseconds
+        }
+
+        private void btnHeadsetDisconnect_Click(object sender, EventArgs e)
+        {
+
+            thinkGearWrapper.Disconnect(); //close the headset connection
+            txtHeadsetDebug.AppendText("Disconnected." + Environment.NewLine);
+            btnHeadsetDisconnect.Enabled = false; //disable the headset disconnect button
+            btnHeadsetConnect.Enabled = true; //enable the headset connect button
+        }
+
+
+
+
+        private void sendBlink(string blinkValue)
+        {
+
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect(ip, 7777);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+               
+                String str = "";
+                if (check)
+                {
+                    check = false;
+                    str = "on";
+
+                }
+                else
+                {
+                    check = true;
+                    str = "off";
+
+                }
+
+
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendBlink(blink.Text);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect(ip, 7777);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+              
+
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes("close");
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception ee)
+            {
+                Console.WriteLine("Error..... " + ee.StackTrace);
+            }
+
+        }
+
+    }
+}
